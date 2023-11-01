9ae593114a5ede491b9c08609629c0125f6a1970
Iris van Ollefen <irisvanollefen@gmail.com>
2023-11-01 14:13:26 +0100

    added styling to desktop work cards

--- app/_components/my-work.jsx
+++ app/_components/my-work.jsx
@@ -7,18 +7,26 @@ export default function MyWork(workCardData) {
       <h3 className='text-2xl font-medium pb-2 flex justify-center'>
         Mijn werk
       </h3>
-      <div>
+      <div className='mt-8 grid grid-cols-2 gap-4'>
         {workCardData.workCardData.map((workCard, index) => {
           return (
             <ul key={index}>
-              <li>{workCard.productName}</li>
-              <li>{workCard.companyOrClient}</li>
-              <li>{workCard.typeOfProduct}</li>
-              <li>
-                <Link href={`/${workCard.productName}`}>
-                  Lees over dit product
-                </Link>
-              </li>
+              <div className='absolute opacity-0 hover:opacity-100 transition duration-500 ease-in-out flex flex-col gap-1 items-center bg-opacity-75 bg-black w-[500px] h-[333px] justify-center text-white shadow-2xl'>
+                <li className='text-lg font-semibold'>
+                  {workCard.productName}
+                </li>
+                <li className='font-light italic'>{workCard.typeOfProduct}</li>
+                <li className='font-semibold pb-2'>
+                  {workCard.companyOrClient}
+                </li>
+                <li>
+                  <Link
+                    href={`/${workCard.productName}`}
+                    className='text-lg font-semibold hover:text-sky-400 active:text-sky-600'>
+                    Lees over dit product
+                  </Link>
+                </li>
+              </div>
               <li>
                 {workCard.image.map((image) => {
                   return (
@@ -28,7 +36,7 @@ export default function MyWork(workCardData) {
                       alt={image.image.responsiveImage.alt}
                       width={image.image.responsiveImage.width}
                       height={image.image.responsiveImage.height}
-                      className='w-1/2'
+                      className='w-[500px] h-[333px]'
                     />
                   )
                 })}
