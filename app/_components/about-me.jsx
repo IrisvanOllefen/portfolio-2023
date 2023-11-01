a20a1dbae11904441c9c6fee0532b9b6ad450383
Iris van Ollefen <irisvanollefen@gmail.com>
2023-11-01 13:17:59 +0100

    created about me block

---
+++ app/_components/about-me.jsx
@@ -0,0 +1,14 @@
+export default function AboutMe(aboutData) {
+  return (
+    <div className='w-1/2 mt-24'>
+      <h3 className='text-2xl font-medium pb-2 flex justify-center'>
+        {aboutData.aboutData.title}
+      </h3>
+      <div className='flex flex-col gap-4'>
+        {aboutData.aboutData.textBlocks.map((textBlock, index) => {
+          return <p key={index}>{textBlock.textBlock}</p>
+        })}
+      </div>
+    </div>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import { performRequest } from 'lib/datocms'
 
 import Hero from './_components/hero'
+import AboutMe from './_components/about-me'
 
 const DATOCMS_QUERY = `
   query heroPage {
@@ -18,17 +19,24 @@ const DATOCMS_QUERY = `
       title
       undertitle
     }
+    aboutPage {
+      title
+      textBlocks {
+        textBlock
+      }
+    }
   }
 `
 
 export default async function Home() {
   const {
-    data: { heroPage },
+    data: { heroPage, aboutPage },
   } = await performRequest({ query: DATOCMS_QUERY })
 
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <Hero heroData={heroPage} />
+      <AboutMe aboutData={aboutPage} />
     </main>
   )
 }
